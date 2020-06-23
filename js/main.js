99ee7b474074bd5e16152ecda1abf978488d3422
Alex <57875454+alexkobrin@users.noreply.github.com>
2020-06-23 22:41:41 +0200

    add file to project

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function(){
+    $('.fairy-tail__slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow:'<button type="button" class=" slick-btn slick-prev">  <img src="images/prev.svg" alt=""> </button>',
+        nextArrow:'<button type="button" class=" slick-btn slick-next"><img src="images/next.svg" alt=""></button>',
+        fade: true,
+        autoplay:true
+      });
+      $('.our-trip__slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow:'<button type="button" class=" slick-btn slick-prev">  <img src="images/prev.svg" alt=""> </button>',
+        nextArrow:'<button type="button" class=" slick-btn slick-next"><img src="images/next.svg" alt=""></button>',
+        fade: true,
+        autoplay:true
+      });
+
+
+
+});
